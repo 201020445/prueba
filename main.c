362225b2870cba3d9fb58a1f3737d349e5745b7b
201020445 <slayeredle@hotmail.es>
2016-08-01 15:26:43 -0600

    codigo

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char comando[100];
+
+void analizar(char c[100]){
+
+
+}
+
+int main()
+{
+    printf("Ingresar codigo:");
+    scanf("%s",comando);
+
+    analizar(comando);
+    return 0;
+}
